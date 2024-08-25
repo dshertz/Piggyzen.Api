8b54eda3a7a71f9bced7434fb9ee0816e7f065b3
Daniel Hertz <112414476+dshertz@users.noreply.github.com>
2024-08-25 12:33:37 +0200

    Update Transaction.cs

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -1,13 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace piggyzen.api.Models
 {
     public class Transaction
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public DateOnly BookingDate { get; set; }
+        public DateOnly TransactionDate { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public decimal Balance { get; set; }
+        public string BookTransDescAmountBalanceID { get; set; }
+        public int CategoryId { get; set; }
     }
 }
