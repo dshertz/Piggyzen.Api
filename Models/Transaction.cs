8e1af7f3f1c4c7eda95dbcdbd0cd24f2ec78357e
Daniel Hertz <112414476+dshertz@users.noreply.github.com>
2024-08-25 11:45:48 +0200

    Create Transaction.cs

---
+++ Models/Transaction.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace piggyzen.api.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
